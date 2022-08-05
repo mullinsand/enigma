3f78d6adbed861f758b3e1e25c49cc48cf4fa5a0
Andrew Mullins <mullinsand15@gmail.com>
2022-08-04 20:49:04 -0600

    feat: add a,b,c,d final shift methods and add character_set attribute and tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,8 @@
 class Enigma
   attr_accessor :message,
               :given_key,
-              :given_date
+              :given_date,
+              :character_set
 
   def initialize
     #component_creation
@@ -14,6 +15,11 @@ class Enigma
     @message = nil
     @given_key = nil
     @given_date = nil
+    @character_set = ["a", "b", "c", "d", "e", "f", 
+                      "g", "h", "i", "j", "k", "l", 
+                      "m", "n", "o", "p", "q", "r", 
+                      "s", "t", "u", "v", "w", "x", 
+                      "y", "z", " "]
   end
 
   def encrypt(message, given_key, given_date = Time.new.strftime("%m%d%y"))
@@ -61,5 +67,21 @@ class Enigma
 
   def d_date_shift
     square_date.digits[0]
+  end
+
+  def a_final_shift
+    a_key_shift + a_date_shift
+  end
+
+  def b_final_shift
+    b_key_shift + b_date_shift
+  end
+
+  def c_final_shift
+    c_key_shift + c_date_shift
+  end
+
+  def d_final_shift
+    d_key_shift + d_date_shift
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,6 +8,11 @@ describe Enigma do
     expect(enigma.message).to eq(nil)
     expect(enigma.given_key).to eq(nil)
     expect(enigma.given_date).to eq(nil)
+    expect(enigma.character_set).to eq(["a", "b", "c", "d", "e", "f", 
+                                        "g", "h", "i", "j", "k", "l", 
+                                        "m", "n", "o", "p", "q", "r", 
+                                        "s", "t", "u", "v", "w", "x", 
+                                        "y", "z", " "])
   end
 
   it 'encrypts a message when given a key and date' do
@@ -82,5 +87,15 @@ describe Enigma do
     allow(enigma).to receive(:encrypt_message).and_return("keder ohulw")
     enigma.encrypt("hello world", "02715", "040895")
     expect(enigma.d_date_shift).to eq(5)
+  end
+
+  it 'determines final shifts for A, B, C, D keys' do
+    allow(enigma).to receive(:encrypt_message).and_return("keder ohulw")
+    enigma.encrypt("hello world", "02715", "040895")
+
+    expect(enigma.a_final_shift).to eq(3)
+    expect(enigma.b_final_shift).to eq(27)
+    expect(enigma.c_final_shift).to eq(73)
+    expect(enigma.d_final_shift).to eq(20)
   end
 end
