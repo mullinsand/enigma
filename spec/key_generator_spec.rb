4fceea551657977cd2eaf64601674cbf1b2a428c
Andrew Mullins <mullinsand15@gmail.com>
2022-08-05 11:07:13 -0500

    feat: update test on key_generator

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -5,7 +5,7 @@ describe KeyGenerator do
   it 'generates a random 5 digit key as a string' do
     key = enigma.generate_key
     expect(key.length).to eq(5)
+    expect(key.to_i).to be_instance_of(Integer)
     expect(key).to be_instance_of(String)
-    require 'pry'; binding.pry
   end
 end
