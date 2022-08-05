8c2619fa53b16ff936f6cf64fb3a951c15cb2eb3
Andrew Mullins <mullinsand15@gmail.com>
2022-08-04 20:24:35 -0600

    feat: add instance variables to enigma class with tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,8 @@
 class Enigma
-  attr_reader
+  attr_accessor :message,
+              :given_key,
+              :given_date
+
   def initialize
     #component_creation
     #CLI Class
@@ -8,13 +11,23 @@ class Enigma
       #Sends info onto crypto class
       #Outputs final stuff
     #Enigma Class
+    @message = nil
+    @given_key = nil
+    @given_date = nil
   end
 
-  def encrypt(message, given_key, given_date)
+  def encrypt(message, given_key, given_date = Time.new.strftime("%m%d%y"))
+    @message = message
+    @given_key = given_key
+    @given_date = given_date
     {
       encryption: encrypt_message(message, given_key, given_date),
       key: given_key,
       date: given_date
     }
+  end
+
+  def a_key_shift
+
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,20 +2,35 @@ require 'enigma'
 
 describe Enigma do
   let(:enigma) {Enigma.new}
+
   it 'exists and has attributes' do
     expect(enigma).to be_instance_of(Enigma)
+    expect(enigma.message).to eq(nil)
+    expect(enigma.given_key).to eq(nil)
+    expect(enigma.given_date).to eq(nil)
   end
 
   it 'encrypts a message when given a key and date' do
-    allow(enigma).to receive(:encrypt).and_return({
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    })
+    allow(enigma).to receive(:encrypt_message).and_return("keder ohulw")
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq( {
           encryption: "keder ohulw",
           key: "02715",
           date: "040895"
         } )
+    expect(enigma.message).to eq("hello world")
+    expect(enigma.given_key).to eq("02715")
+    expect(enigma.given_date).to eq("040895")
+  end
+
+  it 'assigns message, key, and date attributes based on input' do
+    allow(enigma).to receive(:encrypt_message).and_return("keder ohulw")
+    enigma.encrypt("hello world", "02715", "040895")
+    expect(enigma.message).to eq("hello world")
+    expect(enigma.given_key).to eq("02715")
+    expect(enigma.given_date).to eq("040895")
+  end
+
+  it 'determines the key shift for A key' do
+    expect(enigma.a_key_shift).to eq(2)
   end
 end
